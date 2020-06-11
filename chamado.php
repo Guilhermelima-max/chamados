ad7e58eec3e84098a1b79fdff387e19e0568067a
Yan Santos <yannsantos4@gmail.com>
2020-06-11 01:30:42 -0300

    Arquivos php

---
+++ chamado.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  	<head>
+    	<meta charset="UTF-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<title>Levpet</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<a href="index.html">
+			<button class="voltar">Voltar</button>
+		</a>
+		
+		<div id="all">
+
+			<div class="box" id="box2">
+
+				<p class="light">Chamado</p>
+
+				<form id="main_content" method="POST">
+					<div id="form_part1">
+			        	<div>
+							<label class="light" for="nome">Nome:</label>
+				        	<br>
+				          	<input type="text" required id="nome" maxlength="45" placeholder="Digite seu nome">
+						</div>
+						<div>
+							<label class="light" for="empresa">Empresa:</label>
+				        	<br>
+				          	<input type="text" required id="empresa" maxlength="45" placeholder="Digite o nome da empresa">
+						</div>
+						<div id="setor_assunto">
+							<div>
+								<label class="light" for="setor">Setor:</label>
+								<br>
+							    <select name="setor" id="setor" required>
+							    	<option class="dark" value="" selected>Selecione</option>
+								    <option class="dark" value="administracao">Administração</option>
+								    <option class="dark" value="financeiro">Financeiro</option>
+								    <option class="dark" value="marketing">Marketing</option>
+								    <option class="dark" value="logistica">Logística</option>
+								    <option class="dark" value="outros">Outros</option>
+							    </select>
+							</div>
+				          	<div>
+								<label class="light" for="assunto">Assunto:</label>
+								<br>
+							    <select name="assunto" id="assunto" required>
+							    	<option class="dark" value="" selected>Selecione</option>
+								    <option class="dark" value="duvida">Dúvida</option>
+								    <option class="dark" value="reclamacao">Reclamação</option>
+								    <option class="dark" value="outros">Outros</option>
+							    </select>
+							</div>
+						</div>
+						<div>
+			          		<label class="light" for="mensagem">Ocorrência:</label>
+				          	<br>
+				          	<textarea id="mensagem" required maxlength="200" placeholder="Digite sua mensagem"></textarea>
+			          	</div>
+						<div>
+			          		<label class="light" for="sugestao">Sugestão:</label>
+				          	<br>
+				          	<textarea id="sugestao" maxlength="100" placeholder="Digite sua mensagem"></textarea>
+			          	</div>
+			          	<div>
+			          		<button class="dark" type="submit">Enviar</button>
+			          	</div>
+					</div>
+
+			    </form>
+
+			</div>
+			
+		</div>
+
+	</body>
+</html>
---
+++ ver_chamado.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  	<head>
+    	<meta charset="UTF-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    	<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<title>Levpet</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+		<a href="index.html">
+			<button class="voltar">Voltar</button>
+		</a>
+
+		<div id="all2">
+
+			<div class="box3 box">
+				<button class="concluir">Concluir</button>
+				<p class="light dados">Luiza</p>
+				<p class="light dados">Magazine Luiza</p>
+				<p class="light dados">Reclamação</p>
+				<br>
+				<p class="light ocorrencia">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			</div>
+
+			<div class="box3 box">
+				<button class="concluir">Concluir</button>
+				<p class="light dados">Luiza</p>
+				<p class="light dados">Magazine Luiza</p>
+				<p class="light dados">Sugestão</p>
+				<br>
+				<p class="light ocorrencia">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			</div>
+
+			<div class="box3 box">
+				<button class="concluir">Concluir</button>
+				<p class="light dados">Luiza</p>
+				<p class="light dados">Magazine Luiza</p>
+				<p class="light dados">Outros</p>
+				<br>
+				<br>
+				<p class="light ocorrencia">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			</div>
+
+			<div class="box3 box">
+				<button class="concluir">Concluir</button>
+				<p class="light dados">Luiza</p>
+				<p class="light dados">Magazine Luiza</p>
+				<p class="light dados">Reclamação</p>
+				<br>
+				<br>
+				<p class="light ocorrencia">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			</div>
+			
+		</div>
+
+	</body>
+</html>
