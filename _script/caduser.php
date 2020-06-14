10244ee76f033715f691e1d979bd18fe0ac0edfd
Silasoli <62566929+Silasoli@users.noreply.github.com>
2020-06-14 00:12:50 -0300

    final

--- _script/caduser.php
+++ _script/caduser.php
@@ -13,7 +13,7 @@ $sugestao=$_POST['sugestao'];
 
 
 
-//seleciona tudo da tbl usuario onde cpf seja igual ao cpf digitado pelo usuarios
+//seleciona tudo da tbl usuario onde cpf seja igual ao cpf digitado pelo usuario
 $cpfquery=mysqli_query($conn,"select * from tbl_usuario where cpf = '$cpf'");
 $numRegistros = mysqli_num_rows($cpfquery);
 //caso não exista um usuario com esse cfp
@@ -26,8 +26,8 @@ if( $numRegistros == 0){
   $dadoidcpf = mysqli_fetch_array($idquery);
   $iduser = $dadoidcpf['id_usuario'];
 
-  $regoc="INSERT INTO tbl_ocorrencia(assunto,desc_ocorrencia,sugestao,fkEmpresaid,fkUsuarioid,estado) values 
-  ('$assunto','$ocorrencia','$sugestao','$empresa','$iduser','0')";
+  $regoc="INSERT INTO tbl_ocorrencia(assunto,desc_ocorrencia,sugestao,fkEmpresaid,fkUsuarioid) values 
+  ('$assunto','$ocorrencia','$sugestao','$empresa','$iduser')";
   $regocresult=mysqli_query($conn,$regoc) or die('Query failed: ' . mysqli_error($conn));
   
 
@@ -41,8 +41,8 @@ $idquery=mysqli_query($conn,"SELECT * FROM tbl_usuario where cpf = '{$cpf}'");
 $dadoidcpf = mysqli_fetch_array($idquery);
 $iduser = $dadoidcpf['id_usuario'];
 $idemp=$dadoidcpf['fkEmpresa'];
-$regoc="INSERT INTO tbl_ocorrencia(assunto,desc_ocorrencia,sugestao,fkEmpresaid,fkUsuarioid,estado) values 
-('$assunto','$ocorrencia','$sugestao','$idemp','$iduser','0')";
+$regoc="INSERT INTO tbl_ocorrencia(assunto,desc_ocorrencia,sugestao,fkEmpresaid,fkUsuarioid) values 
+('$assunto','$ocorrencia','$sugestao','$idemp','$iduser')";
 $regocresult=mysqli_query($conn,$regoc) or die('Query failed: ' . mysqli_error($conn));
   
 }   
